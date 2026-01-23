09baa0ac2280980b7ecefc47ece45149c499d886
Tim Hsu <maochindada@gmail.com>
2026-01-23 22:56:54 +0800

    feat: add users table migration

--- processor/migrations/phoenix/0001_phoenix.up.sql
+++ processor/migrations/phoenix/0001_phoenix.up.sql
@@ -1,3 +1,10 @@
 BEGIN;
-COMMIT;
 
+CREATE TABLE IF NOT EXISTS users (
+    username      TEXT PRIMARY KEY,
+    password_hash TEXT NOT NULL,
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+COMMIT;
